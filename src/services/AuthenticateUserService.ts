917bf40522cc84bc0da4b09a507622689a14fa7b
FrostPriest <johnericjahn@gmail.com>
2020-06-25 11:20:16 -0300

    Generating JWT Token

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,5 +1,7 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
+
 import User from '../models/User';
 
 interface Request {
@@ -7,11 +9,13 @@ interface Request {
     password: string;
 }
 
+interface Response {
+    user: User;
+    token: string;
+}
+
 class AuthenticateUserService {
-    public async execute({
-        email,
-        password,
-    }: Request): Promise<{ user: User }> {
+    public async execute({ email, password }: Request): Promise<Response> {
         const usersRepository = getRepository(User);
 
         const user = await usersRepository.findOne({
@@ -28,8 +32,14 @@ class AuthenticateUserService {
             throw new Error('Incorrect email/password combination.');
         }
 
+        const token = sign({}, 'd5e454048d7d32157acfe9f5849c9bd7', {
+            subject: user.id,
+            expiresIn: '1d',
+        });
+
         return {
             user,
+            token,
         };
     }
 }
